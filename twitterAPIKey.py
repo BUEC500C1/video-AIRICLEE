e2f879eac32082fa5eba395fa11e4b9cc927ff38
AIRICLEE <49914236+AIRICLEE@users.noreply.github.com>
2020-02-22 15:55:51 -0500

    Update twitterAPIKey.py

--- twitterAPIKey.py
+++ twitterAPIKey.py
@@ -1,8 +1,8 @@
 # Enter your API keys here
-consumer_key = "AlSQp3f1uWxA5pQiNP08375jx"
-consumer_secret = "FPb30QV8B2IYRGdbZqTLGsLhMSuMo5VOeMgSuXUPFHD2HOSQPs"
-access_key = "276619791-vnoEqnqkAJhXBEbVbv6UB7Vb3vYxM2use1tofna6"
-access_secret = "sIhPcPoiW0prsxN7lRjJLzYtPcKIsdhAr0TabfAf7Ys2o"
+consumer_key = "**********************"
+consumer_secret = "*************************************************"
+access_key = "***************************************************"
+access_secret = "*******************************************"
 
 # Enter what you want search in twitter
 # Here we search iphone 11 as example
